c65cbdd7592f076f2885289b9ca691d322e73940
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-01-18 22:00:29 -0300

    progress

--- utils.py
+++ utils.py
@@ -1,27 +1,31 @@
-from goodreads_scraper.utils import setup_browser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-url = "https://www.goodreads.com/author/show/13199.Alain_de_Botton"
-browser = setup_browser()
-
-browser.get(url)
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+import re
 
 
-body = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "body"))
-)
+def scrape_gr_author(url: str):
+    browser = webdriver.Chrome()
 
-born_label = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//div[@class='dataTitle' and text()='Born']"))
-)
+    browser.get(url)
 
-birthplace = browser.execute_script(
-    "return arguments[0].nextSibling.textContent.trim();", 
-    born_label
-)
+    body = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "body"))
+    )
+    try:
+        born_label = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[@class='dataTitle' and text()='Born']")
+            )
+        )
 
-print(birthplace)
+        raw_birthplace = browser.execute_script(
+            "return arguments[0].nextSibling.textContent.trim();", born_label
+        )
 
-#TODO: Encapsular, testar, lidar gracefully com caso em que o elemento não existe.
+        birthplace = re.sub(r"^in\s+", "", raw_birthplace)
+    except TimeoutException:
+        birthplace = None
+    return birthplace
