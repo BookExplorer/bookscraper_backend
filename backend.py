7f38e0f49d3685b8bd610026b12195d4b7a5e684
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-02-24 06:28:31 -0300

    country count

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from goodreads_scraper.scrape import process_profile
 from typing import Dict, List
 from collections import Counter
 from utils import scrape_gr_author, cleanup_birthplace
+import pandas as pd
 
 
 def extract_authors(books: List[Dict[str, str]]) -> Counter:
@@ -10,11 +11,9 @@ def extract_authors(books: List[Dict[str, str]]) -> Counter:
     return author_tuple_counts
 
 
-if __name__ == "__main__":
-    user_profile = "https://www.goodreads.com/user/show/71341746-tamir-einhorn-salem"
-    books = process_profile(user_profile)
-    cont = extract_authors(books)
+def generate_country_count(cont: Counter):
     author_info_dict = {}
+    country_counter = {}
     for (author_id, author_link), count in cont.items():
         birthplace = scrape_gr_author(author_link)  # Scrape the birthplace
         country = cleanup_birthplace(birthplace)
@@ -24,4 +23,17 @@ if __name__ == "__main__":
             "birthplace": birthplace,
             "country": country,
         }
-        print(2)
+        if country and country in country_counter:
+            country_counter[country] += 1
+        elif country:
+            country_counter[country] = 1
+    return country_counter
+
+
+if __name__ == "__main__":
+    user_profile = "https://www.goodreads.com/user/show/71341746-tamir-einhorn-salem"
+    books = process_profile(user_profile)
+    cont = extract_authors(books)
+    cc = generate_country_count(cont)
+    df = pd.DataFrame(list(cc.items()), columns=["Country", "Count"])
+    print(df)
