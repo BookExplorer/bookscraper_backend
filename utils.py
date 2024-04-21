83bed7a791c21c12b8cba3ff023cef0ed0a58e31
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-04-20 22:24:41 -0300

    Documents utils

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from goodreads_scraper.utils import setup_browser
 import re
 
 
-def scrape_gr_author(url: str):
+def scrape_gr_author(url: str) -> str | None:
+    """
+    Scrapes the author's Goodreads page and extracts the author's birthplace.
+    Args:
+        url (str): The author's Goodreads URL, brought from the scraping of the user's page.
+
+    Returns:
+        str | None: Birthplace of the author or None if we can't find it in the authors page.
+    """
+
     browser = setup_browser()
 
     browser.get(url)
@@ -32,7 +40,15 @@ def scrape_gr_author(url: str):
     return birthplace
 
 
-def cleanup_birthplace(birthplace: str):
+def cleanup_birthplace(birthplace: str | None) -> str | None:
+    """Cleans up the birthplace string that comes from Goodreads.
+
+    Args:
+        birthplace (str | None): Birthplace scraped from Goodreads.
+
+    Returns:
+        str | None: Cleaned up birthplace if applicable, None if birthplace is None.
+    """
     if birthplace:
         raw_country = birthplace.split(",")[-1].lstrip()
         if raw_country.startswith("The "):
