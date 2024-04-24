215f2e183e1315448b443c54b6f171fd015ea8d5
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-04-23 22:05:18 -0300

    Fixture for running alembic

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,23 @@
 from fastapi.testclient import TestClient
 from backend_api import app
+from pytest import fixture
+from alembic.config import Config
+from alembic import command
+
 
 client = TestClient(app)
+
+
+@fixture(scope="session", autouse=True)
+def apply_migrations():
+    # Setup: Run migrations
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+    yield
+
+    # Teardown: Optionally, you can downgrade or drop tables
+    command.downgrade(alembic_cfg, "base")
 
 
 def test_profile_endpoint():
