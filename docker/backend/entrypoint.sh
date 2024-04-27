d5b41eed2866999d267f29950e1b027f7e63c829
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-04-27 04:13:57 -0300

    entrypoint modifications

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Run Alembic upgrade to migrate the database
+# Wait for the PostgreSQL database to be ready
+echo "Waiting for PostgreSQL to start..."
+while ! nc -z db 5432; do
+  sleep 1
+done
+echo "PostgreSQL is up - executing command"
+
+# Run Alembic migrations
+echo "Running Alembic upgrade"
 poetry run alembic upgrade head
 
 # Start the Uvicorn server
+echo "Starting Uvicorn server"
 exec poetry run uvicorn backend_api:app --host 0.0.0.0 --port 8000
