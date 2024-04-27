c42ab76c7e496c137fecd794a7576899c9eef6c0
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-04-26 21:29:39 -0300

    URL validation via Pydantic

--- backend_api.py
+++ backend_api.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, HttpUrl
 
 
 class ProfileRequest(BaseModel):
-    profile_url: str
+    profile_url: HttpUrl
 
 
 app = FastAPI()
@@ -18,10 +18,11 @@ app = FastAPI()
 @app.post("/process-profile/")
 def profile(request: ProfileRequest):
     try:
-        books = process_profile(request.profile_url)
+        books = process_profile(str(request.profile_url))
         cont = extract_authors(books)
         cc = generate_country_count(cont)
         full_count = process_country_count(cc)
         return {"data": full_count}
     except Exception as e:
+        print(str(e))
         raise HTTPException(status_code=500, detail=str(e))
