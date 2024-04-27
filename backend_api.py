ddef9198cd33bae3362c23428ad4b238fa6f8ac2
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-04-27 02:21:56 -0300

    Adds logs to API

--- backend_api.py
+++ backend_api.py
@@ -6,6 +6,11 @@ from backend import (
     generate_country_count,
 )
 from pydantic import BaseModel, HttpUrl
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class ProfileRequest(BaseModel):
@@ -17,12 +22,17 @@ app = FastAPI()
 
 @app.post("/process-profile/")
 def profile(request: ProfileRequest):
+    logging.info(f"Received profile URL: {request.profile_url}")
     try:
         books = process_profile(str(request.profile_url))
+        logging.info("Profile processed successfully.")
         cont = extract_authors(books)
+        logging.info("Authors extracted!")
         cc = generate_country_count(cont)
+        logging.info("Country count generated")
         full_count = process_country_count(cc)
+        logging.info("Full country count ready")
         return {"data": full_count}
     except Exception as e:
-        print(str(e))
+        logging.error(f"Error processing profile: {str(e)}", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
