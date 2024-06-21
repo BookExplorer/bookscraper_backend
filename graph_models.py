9f0bddd7651e47c0709a621a816fd62026083bb3
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-06-21 16:41:11 -0300

    improve models

--- graph_models.py
+++ graph_models.py
@@ -9,24 +9,33 @@ from neomodel import (
 )
 
 
+# TODO: This has no constraints and so we need to deal with them elsewhere.
 class Country(StructuredNode):
+    uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
+    regions = RelationshipTo("Region", "CONTAINS")
+    cities = RelationshipTo("City", "CONTAINS")
 
 
 class Region(StructuredNode):
+    # This is states or counties or whatever.
+    uid = UniqueIdProperty()
     name = StringProperty(required=True)
     country = RelationshipTo(Country, "WITHIN", cardinality=One)
 
 
 class City(StructuredNode):
+    uid = UniqueIdProperty()
     name = StringProperty(required=True)
     # If a region exists
     region = RelationshipTo(Region, "WITHIN", cardinality=ZeroOrOne)
-    country = RelationshipTo(Country, "WITHIN", cardinality=One)
+    country = RelationshipTo(Country, "WITHIN", cardinality=ZeroOrOne)
 
 
 class Author(StructuredNode):
     uid = UniqueIdProperty()
     goodreads_id = StringProperty(unique_index=True)
+    goodreads_link = StringProperty()
     name = StringProperty(required=True)
-    city = RelationshipTo(City, cardinality=ZeroOrOne)
+    birth_city = RelationshipTo(City, "BORN_IN", cardinality=ZeroOrOne)
+    lived_in = RelationshipTo(City, "LIVED_IN")
