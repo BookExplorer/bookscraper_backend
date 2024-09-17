ade8653658b94e2d799364a911bf8096ce7bff00
Tamir <tamtamireinhorn@gmail.com>
2024-09-17 17:54:21 -0300

    wrong url

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ def test_profile_endpoint():
     response = client.post(
         "/process-profile/",
         json={
-            "profile_url": "https://www.goodreads.com/user/show/6681479-lucas-pavanelli"
+            "profile_url": "https://www.goodreads.com/user/show/66818479-lucas-pavanelli"
         },
     )
 
