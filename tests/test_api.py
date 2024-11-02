7b7582c24d83d5731230ac4c9fbc55c8b5b84e7c
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-11-02 18:49:51 -0300

    more robust test

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,11 +12,16 @@ def test_profile_endpoint():
         response = client.post(
             "/process-profile/",
             json={
-                "profile_url": "https://www.goodreads.com/user/show/66818479-lucas-pavanelli"
+                "profile_url": "https://www.goodreads.com/user/show/183326807"
             },
         )
 
     assert response.status_code == 200
+    # In this profile, we have just 2 books, one from Orwell (India) and Twain (USA)
+    # For stupid reasons, the USA from Goodreads fails. So we expect india = 1, rest 0.
+    data =  response.json()["data"]
+    assert data["India"] == 1
+    assert all(data[country] == 0 for country in data if country != "India")
 
 
 def test_profile_endpoint_bad_url():
