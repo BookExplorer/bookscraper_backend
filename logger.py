822f54534fed809f42e12ed6e3382d33c78ab351
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2025-04-23 20:33:52 -0300

    log level info

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import sys
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler(sys.stdout)])
     
