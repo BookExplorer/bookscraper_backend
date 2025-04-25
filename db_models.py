8d80ce97edab28aeed8dd6912b8b9f8f890d5394
Tamir <tamtamireinhorn@gmail.com>
2025-04-25 11:37:12 -0300

    basic tables

--- db_models.py
+++ db_models.py
@@ -1,13 +1,31 @@
 from typing import List
 from typing import Optional
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
 
 
 class Base(DeclarativeBase):
     pass
 
+class Countries(Base):
+    __tablename__ = "countries"
+    country_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    country_name: Mapped[str]
+
+class Region(Base):
+    __tablename__ = "regions"
+    region_id: Mapped[int] = mapped_column(primary_key=True)
+    region_name: Mapped[str]
+
+class City(Base):
+    __tablename__ = "cities"
+    city_id: Mapped[int] = mapped_column(primary_key=True)
+    city_name: Mapped[str]
+
+class Author(Base):
+    __tablename__ = "authors"
+    author_id: Mapped[int] = mapped_column(primary_key=True)
+    author_name: Mapped[str]
+    goodreads_id: Mapped[int]
+    goodreads_link: Mapped[str]
