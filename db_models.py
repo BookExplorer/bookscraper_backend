f0b6929b76e428fa8febb39672681d954d9a88c9
Tamir <tamtamireinhorn@gmail.com>
2025-04-25 19:41:34 -0300

    db models with sql lite

--- db_models.py
+++ db_models.py
@@ -1,31 +1,59 @@
-from typing import List
 from typing import Optional
-from sqlalchemy import String, Integer, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped,sessionmaker, mapped_column, relationship
 
+db_url = 'sqlite:///t.db'
+
+engine = create_engine(db_url)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 class Base(DeclarativeBase):
     pass
 
-class Countries(Base):
+class BaseModel(Base):
+    __abstract__ = True # This allows it to be inherited by other classes.
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    def __repr__(self):
+        return f"Class: {type(self).__name__} Id: {self.id}, name: {self.name}"
+    
+class Country(BaseModel):
     __tablename__ = "countries"
-    country_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    country_name: Mapped[str]
+    regions: Mapped[list["Region"]] = relationship(back_populates="country") # Writes to Region.country
 
-class Region(Base):
+
+class Region(BaseModel):
     __tablename__ = "regions"
-    region_id: Mapped[int] = mapped_column(primary_key=True)
-    region_name: Mapped[str]
+    country_id: Mapped[Optional[int]] = mapped_column(ForeignKey(Country.id))
+    country: Mapped[Country] = relationship(back_populates="regions")
+    cities: Mapped[Optional[list["City"]]] = relationship(back_populates="region")
 
-class City(Base):
+class City(BaseModel):
     __tablename__ = "cities"
-    city_id: Mapped[int] = mapped_column(primary_key=True)
-    city_name: Mapped[str]
+    region_id: Mapped[Optional[int]] = mapped_column(ForeignKey(Region.id))
+    region: Mapped[Optional["Region"]] = relationship(back_populates="cities")
+    country_id: Mapped[Optional[int]] = mapped_column(ForeignKey(Country.id))
+    authors: Mapped[Optional[list["Author"]]] = relationship(back_populates="city")
 
-class Author(Base):
+class Author(BaseModel):
     __tablename__ = "authors"
-    author_id: Mapped[int] = mapped_column(primary_key=True)
-    author_name: Mapped[str]
-    goodreads_id: Mapped[int]
-    goodreads_link: Mapped[str]
+    goodreads_id: Mapped[Optional[int]]
+    goodreads_link: Mapped[Optional[str]]
+    birth_city_id = mapped_column(ForeignKey(City.id))
+    city: Mapped[City] = relationship(back_populates="authors")
+
+Base.metadata.create_all(engine)
+brasil = Country(name="Brasil")
+rio = Region(name="Rio de Janeiro", country=brasil)
+tere = City(name="Teresópolis", region = rio)
+ni = City(name="Nova Iguacu", region = rio)
+a = Author(name= "ze", city = ni)
+b = Author(name = "cuin",city=tere)
+c = Author(name= "mr", city=ni)
+session.add(brasil)
+session.commit()
+print(brasil)
+
