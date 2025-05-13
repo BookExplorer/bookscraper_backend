8fa16427938c2e5a687680511b78b7138e1da61b
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2025-05-13 02:03:26 -0300

    tests incomplete

--- bookscraper_backend/tests/test_db_models.py
+++ bookscraper_backend/tests/test_db_models.py
@@ -2,12 +2,35 @@ from hypothesis import given, assume, strategies as st
 import string
 import pytest
 from sqlalchemy.exc import IntegrityError
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from testcontainers.postgres import PostgresContainer
 from bookscraper_backend.database import db_models
-from bookscraper_backend.database.setup import db_session
+from bookscraper_backend.database.setup import db_session,db_url
+from alembic.config import Config
+from alembic import command
 
 naming_strategy = st.text(alphabet=string.ascii_letters + " -", min_size=1)
 
+@pytest.fixture(scope="module", autouse=True)
+def postgres_container(request):
+    with PostgresContainer() as postgres:
+        postgres.start()
 
+        def remove_container():
+            postgres.stop()
+
+        request.addfinalizer(remove_container)  
+        engine = sa.create_engine(postgres.get_connection_url())
+        alembic_cfg = Config("alembic.ini")
+        alembic_cfg.set_main_option("sqlalchemy.url", postgres.get_connection_url())
+        command.upgrade(alembic_cfg, "head")
+        SessionLocal = sessionmaker(bind=engine)
+        session = SessionLocal()
+
+        yield session
+
+        session.close()
 
 @st.composite
 def valid_existing_country(draw):
@@ -28,3 +51,10 @@ def valid_former_country(draw):
 def invalid_former_country(draw):
     name = draw(naming_strategy)
     return db_models.Country(name=name, still_exists=False, end_date = None)
+
+@given(valid_existing_country())
+def test_valid_country(country: db_models.Country) -> None:
+    db_session.add(country)
+    db_session.commit()
+    assert country.id is not None
+
