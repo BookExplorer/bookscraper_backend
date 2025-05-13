d3a1724f5356e545a9bcbc0efbc2919444f448c2
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2025-05-12 22:39:39 -0300

    define composite strategies

--- bookscraper_backend/tests/test_db_models.py
+++ bookscraper_backend/tests/test_db_models.py
@@ -5,21 +5,26 @@ from sqlalchemy.exc import IntegrityError
 from bookscraper_backend.database import db_models
 from bookscraper_backend.database.setup import db_session
 
-#TODO: Maybe define from this invalid countries and valid countries and all that.
-@given(
-    name=st.text(alphabet=string.ascii_letters + " -", min_size=1, max_size=50),
-    still_exists=st.booleans(),
-    end_date=st.one_of(st.none(), st.dates())
-)
-def test_country_constraints(name, still_exists, end_date):
-    c = db_models.Country(name=name, still_exists=still_exists, end_date=end_date)
-    db_session.add(c)
-    should_fail = (still_exists and end_date is not None) or (not still_exists and end_date is None)
+naming_strategy = st.text(alphabet=string.ascii_letters + " -", min_size=1)
 
-    try:
-        db_session.commit()
-    except IntegrityError:
-        db_session.rollback()
-        assert should_fail, f"Expected the bad combo ({still_exists=}, {end_date=}) to fail"
-    else:
-        assert not should_fail, f"Expected the good combo ({still_exists=}, {end_date=}) to pass"
+
+
+@st.composite
+def valid_existing_country(draw):
+    name = draw(naming_strategy)
+    return db_models.Country(name=name, still_exists=True, end_date = None)
+
+@st.composite
+def invalid_existing_country(draw):
+    name = draw(naming_strategy)
+    return db_models.Country(name=name, still_exists=True, end_date = draw(st.dates()))
+
+@st.composite
+def valid_former_country(draw):
+    name = draw(naming_strategy)
+    return db_models.Country(name=name, still_exists=False, end_date = draw(st.dates()))
+
+@st.composite
+def invalid_former_country(draw):
+    name = draw(naming_strategy)
+    return db_models.Country(name=name, still_exists=False, end_date = None)
