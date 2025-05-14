b237448e743ee19c9bb1b55314b885402e4f8e73
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2025-05-14 02:09:30 -0300

    wip

--- bookscraper_backend/tests/test_db_models.py
+++ bookscraper_backend/tests/test_db_models.py
@@ -3,17 +3,17 @@ import string
 import pytest
 from sqlalchemy.exc import IntegrityError
 import sqlalchemy as sa
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from testcontainers.postgres import PostgresContainer
 from bookscraper_backend.database import db_models
-from bookscraper_backend.database.setup import db_session,db_url
+from bookscraper_backend.database.setup import db_session
 from alembic.config import Config
 from alembic import command
 
 naming_strategy = st.text(alphabet=string.ascii_letters + " -", min_size=1)
 
 @pytest.fixture(scope="module", autouse=True)
-def postgres_container(request):
+def postgres_container(request) -> Session:
     with PostgresContainer() as postgres:
         postgres.start()
 
@@ -23,7 +23,7 @@ def postgres_container(request):
         request.addfinalizer(remove_container)  
         engine = sa.create_engine(postgres.get_connection_url())
         alembic_cfg = Config("alembic.ini")
-        alembic_cfg.set_main_option("sqlalchemy.url", postgres.get_connection_url())
+        alembic_cfg.set_main_option("sqlalchemy.url", postgres.get_connection_url()) # I think this is failing now?
         command.upgrade(alembic_cfg, "head")
         SessionLocal = sessionmaker(bind=engine)
         session = SessionLocal()
@@ -31,6 +31,14 @@ def postgres_container(request):
         yield session
 
         session.close()
+
+@pytest.fixture(scope="function", autouse=True)
+def cleanup_tables(postgres_container: Session):
+    meta = sa.MetaData()
+    meta.reflect(bind=postgres_container.bind) #type: ignore
+    for table in reversed(meta.sorted_tables):
+        db_session.execute(sa.text(f'TRUNCATE TABLE "{table.name}" RESTART IDENTITY CASCADE;'))
+
 
 @st.composite
 def valid_existing_country(draw):
