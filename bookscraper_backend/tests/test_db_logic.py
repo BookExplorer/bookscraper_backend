16d6b6185b68afc10cceaa7a8778075b12b014de
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2025-07-27 00:41:50 -0300

    todo

--- bookscraper_backend/tests/test_db_logic.py
+++ bookscraper_backend/tests/test_db_logic.py
@@ -8,7 +8,7 @@ settings.register_profile(
     "my_profile", suppress_health_check=[HealthCheck.function_scoped_fixture]
 )
 settings.load_profile("my_profile")
-
+#TODO: Remove this. Hypothesis is simply too cumbersome for use in this project.
 @given(authors = random_world())
 def test_fetch_all_authors(db_session_factory: SessionFactory, authors: list[db_models.Author]) -> None:
     print(f"Running test with {len(authors)} authors")
