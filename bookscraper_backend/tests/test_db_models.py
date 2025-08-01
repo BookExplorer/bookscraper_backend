5b8f723bf5ae850e6b48ef6dcb97bf72f61e7b30
Tamir <tamtamireinhorn@gmail.com>
2025-08-01 02:27:40 -0300

    2 more to go

--- bookscraper_backend/tests/test_db_models.py
+++ bookscraper_backend/tests/test_db_models.py
@@ -124,8 +124,6 @@ def test_valid_region_creation(db_session_factory: SessionFactory, country: db_m
     A valid region can be safely created.
     """
     with db_session_factory() as db_session:
-        db_session.add(country)
-        db_session.commit()
         region = db_models.Region(name=country.name, country=country)
         db_session.add(region)
         db_session.commit()
@@ -140,8 +138,6 @@ def test_region_unique_constraint(db_session_factory: SessionFactory, country: d
     Regions inside of a country should be unique by name.
     """
     with db_session_factory() as db_session:
-        db_session.add(country)
-        db_session.commit()
         region_1 = db_models.Region(name=country.name, country=country)
         db_session.add(region_1)
         db_session.commit()
@@ -160,101 +156,85 @@ def test_city_check_constraint_nothing(db_session_factory: SessionFactory, count
     A city must be connected to either a region or a country, it cannot be an orphan.
     """
     with db_session_factory() as db_session:
-        db_session.add(country)
-        db_session.commit()
-        region = db_models.Region(name=country.name, country=country)
-        db_session.add(region)
-        db_session.commit()
         city_without_anything = db_models.City(name=country.name)
         db_session.add(city_without_anything)
         with pytest.raises(IntegrityError) as exc:
             db_session.commit()
     assert "cities_check" in str(exc.value)
 
-@given(name=naming_strategy)
-def test_city_check_constraint_both(db_session_factory: SessionFactory, name: str) -> None:
+
+@pytest.mark.parametrize("country", VALID_FORMER_COUNTRIES+VALID_EXISTING_COUNTRIES)
+def test_city_check_constraint_both(db_session_factory: SessionFactory, country: db_models.Country) -> None:
     """
     A city cannot be directly inside of a country and a region. It's one or the other.
     """
-    with db_session_factory() as db_session:
-        country_name = f"country_{name}"
-        region_name = f"region_{name}"
-        city_name = f"city_{name}"    
-        country = db_models.Country(name=country_name, still_exists = True)
-        region = db_models.Region(name=region_name, country=country)
-        city_with_both = db_models.City(name=city_name, country=country, region=region)
+    with db_session_factory() as db_session:   
+        country = db_models.Country(name=country.name, still_exists = True)
+        region = db_models.Region(name=country.name, country=country)
+        city_with_both = db_models.City(name=country.name, country=country, region=region)
         db_session.add(city_with_both)
         with pytest.raises(IntegrityError) as exc:
             db_session.commit()
     assert "cities_check" in str(exc.value)
 
 
-@given(name=naming_strategy)
-def test_city_uq_city_region(db_session_factory: SessionFactory, name: str) -> None:
+
+@pytest.mark.parametrize("country", VALID_FORMER_COUNTRIES+VALID_EXISTING_COUNTRIES)
+def test_city_uq_city_region(db_session_factory: SessionFactory, country: db_models.Country) -> None:
     """Test uniqueness constraint of city.name directly inside of region.
 
     If a city is directly inside of a region, 
     then there should be no other city in that region with the same name.
     """
-    with db_session_factory() as db_session:
-        country_name = f"country_{name}"
-        region_name = f"region_{name}"
-        city_name = f"city_{name}"    
-        country = db_models.Country(name=country_name, still_exists = True)
-        region = db_models.Region(name=region_name, country=country)
-        city_1 = db_models.City(name=city_name, region=region)
-        city_2 = db_models.City(name=city_name, region=region)
+    with db_session_factory() as db_session: 
+        region = db_models.Region(name=country.name, country=country)
+        db_session.add(region)
+        db_session.commit()
+        city_1 = db_models.City(name=country.name, region=region)
+        city_2 = db_models.City(name=country.name, region=region)
         db_session.add(city_1)
         db_session.commit()
         db_session.add(city_2)
         with pytest.raises(IntegrityError) as exc:
             db_session.commit()
-        assert "uq_city_region" in str(exc.value)
+    assert "uq_city_region" in str(exc.value)
 
 
-@given(name=naming_strategy)
-def test_city_uq_city_country(db_session_factory: SessionFactory, name: str) -> None:
+@pytest.mark.parametrize("country", VALID_FORMER_COUNTRIES+VALID_EXISTING_COUNTRIES)
+def test_city_uq_city_country(db_session_factory: SessionFactory, country: db_models.Country) -> None:
     """Test uniqueness constraint of city.name directly inside of country.
 
     If a city is not inside of a region but is directly inside of a country, 
     then there should be no other city in that country with the same name.
     """
-    with db_session_factory() as db_session:
-        country_name = f"country_{name}"
-        city_name = f"city_{name}"    
-        country = db_models.Country(name=country_name, still_exists = True)
-        city_1 = db_models.City(name=city_name, country=country)
-        city_2 = db_models.City(name=city_name, country=country)
+    with db_session_factory() as db_session: 
+        db_session.add(country)
+        db_session.commit()
+        city_1 = db_models.City(name=country.name, country=country)
+        city_2 = db_models.City(name=country.name, country=country)
         db_session.add(city_1)
         db_session.commit()
         db_session.add(city_2)
         with pytest.raises(IntegrityError) as exc:
             db_session.commit()
-        assert "uq_city_country" in str(exc.value)
+    assert "uq_city_country" in str(exc.value)
 
 
-@given(name=naming_strategy)
-def test_linked_creation(db_session_factory: SessionFactory, name: str) -> None:
-    """This test should verify that commiting just an author linked to other classes creates everything.
+@pytest.mark.parametrize("country", VALID_FORMER_COUNTRIES+VALID_EXISTING_COUNTRIES)
+def test_linked_creation(db_session_factory: SessionFactory, country:db_models.Country) -> None:
+    """This test should verify that commiting just an author linked to other classes creates everything IF EVERYTHING IS CORRECT.
 
     What this means is that an author born in a city X, 
     inside region Y, inside country Z, can be solely added and commited 
     without other additions and everything gets created.
     """
-    #This test is weird and actually shrinking, loops back after author??? 
-    #FIXME: Why does shrinkage not fail???
     with db_session_factory() as db_session:
-        country_name = f"country_{name}"
-        region_name = f"region_{name}"
-        city_name = f"city_{name}"
-        author_name = f"author_{name}"
-        country = db_models.Country(name=country_name, still_exists = True)
-        region = db_models.Region(name=region_name, country=country)
-        city = db_models.City(name=city_name, region=region)
-        author = db_models.Author(name=author_name, birth_city=city)
+        region = db_models.Region(name=country.name, country=country)
+        city = db_models.City(name=country.name, region=region)
+        author = db_models.Author(name=country.name, birth_city=city)
         db_session.add(author)
         db_session.commit()
-        other_author = db_models.Author(name=f"{author_name}a", birth_city=city)
+        other_author = db_models.Author(name=f"{country.name}a", birth_city=city)
         db_session.add(other_author)
         db_session.commit()
         assert author.id == 1
@@ -262,6 +242,33 @@ def test_linked_creation(db_session_factory: SessionFactory, name: str) -> None:
         assert region.id ==1
         assert country.id == 1
         assert other_author.id == 2
+
+@pytest.mark.parametrize("country", VALID_FORMER_COUNTRIES+VALID_EXISTING_COUNTRIES)
+def test_linked_creation_repeats(db_session_factory: SessionFactory, country:db_models.Country) -> None:
+    """This test should verify that commiting just an author linked to other classes creates everything IF EVERYTHING IS CORRECT.
+
+    What this means is that an author born in a city X, 
+    inside region Y, inside country Z, can be solely added and commited 
+    without other additions and everything gets created.
+    """
+    #FIXME: Why does this pass
+    with db_session_factory() as db_session:
+        region = db_models.Region(name=country.name, country=country)
+        city = db_models.City(name=country.name, region=region)
+        duplicate_city = db_models.City(name=country.name, region=region)
+        author = db_models.Author(name=country.name, birth_city=city)
+        db_session.add(author)
+        db_session.commit()
+        other_author = db_models.Author(name=f"{country.name}a", birth_city=duplicate_city)
+        db_session.add(other_author)
+        db_session.commit()
+        assert author.id == 1
+        assert city.id == 1
+        assert region.id ==1
+        assert country.id == 1
+        assert other_author.id == 2
+        assert duplicate_city.id == 2
+
 
 @given(authors=random_world())
 def test_regular_creation(db_session_factory: SessionFactory, authors: list[db_models.Author]) -> None:
