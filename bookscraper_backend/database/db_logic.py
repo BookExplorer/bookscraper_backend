dc2ce9ebee1c60d96540099eddc405adc14e58b1
Tamir <tamtamireinhorn@gmail.com>
2025-08-10 19:59:50 -0300

    introduce author dict and some other stuff

--- bookscraper_backend/database/db_logic.py
+++ bookscraper_backend/database/db_logic.py
@@ -5,7 +5,7 @@ import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.dialects.postgresql import insert
 import os
-from typing import Sequence
+from typing import Sequence,TypedDict
 from goodreads_scraper.scrape import process_goodreads_url, scrape_gr_author
 from bookscraper_backend.backend import process_birthplace, GeoDict
 
@@ -23,14 +23,20 @@ DB_URL = sa.URL.create(
 )
 ENGINE = sa.create_engine(DB_URL)
 
+class AuthorDict(TypedDict):
+    name: str
+    goodreads_id: int  | None
+    goodreads_link: str  | None
+
+
 def fetch_all_authors(db_session: Session) -> Sequence[db_models.Author]:
     results = db_session.execute(sa.select(db_models.Author)).scalars().all()
     return results
 
 
-def find_missing_authors(db_authors: Sequence[db_models.Author], scrapped_authors: Counter) -> list[tuple]:
+def find_missing_authors(db_authors: Sequence[db_models.Author], scrapped_authors: Counter) -> list[AuthorDict]:
     existing_ids = [author.goodreads_id for author in db_authors]
-    missing_authors = [item for item in scrapped_authors.items() if item[0][0] not in existing_ids]
+    missing_authors = [AuthorDict(goodreads_id=item[0][0],goodreads_link=item[0][1], name=item[0][2]) for item in scrapped_authors.items() if item[0][0] not in existing_ids]
     return missing_authors
 
 
@@ -69,6 +75,12 @@ def generate_country_count(db_session: Session, books_per_author: Counter):
         gr_id, gr_link, author_name = author[0]
         birthplace, _ = scrape_gr_author(gr_link)
         geo_dict = process_birthplace(birthplace)
-        print(birthplace)
-        # do something
+        city = insert_geo_dict(geo_dict)
+        author_object = db_models.Author(birth_city = city, name = author_name, goodreads_link=gr_link,goodreads_id=gr_id)
+        db_session.add(author_object)
+        db_session.commit()
+    pass
+
+
+def insert_missing_author() -> None:
     pass
